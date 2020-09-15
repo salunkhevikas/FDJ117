057470fbfefdee19d96669f54f08e5a771ae3a59
pradeep <pradeep@XXZZ>
2020-09-15 22:54:03 +0530

    developed by viki

--- GitSampleProject/src/com/cjc/sample/GitSample.java
+++ GitSampleProject/src/com/cjc/sample/GitSample.java
@@ -2,6 +2,10 @@ package com.cjc.sample;
 
 public class GitSample 
 {
+	public void m1()
+	{
+		System.out.println("m1---A");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("GitSample---Main");
